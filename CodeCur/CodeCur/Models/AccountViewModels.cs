813424c831c7dc1871730bcfa8a071bbaf1d24cb
Birkir Karlsson <birkir.k@me.com>
2017-05-02 12:59:09 +0000

    Creating application user correctly

--- CodeCur/CodeCur/Models/AccountViewModels.cs
+++ CodeCur/CodeCur/Models/AccountViewModels.cs
@@ -73,10 +73,6 @@ namespace CodeCur.Models
         public string LastName { get; set; }
 
         [Required]
-        [Display(Name = "Username")]
-        public string Username { get; set; }
-
-        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
