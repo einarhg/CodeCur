2f48d7703e0ef2950c538adc3206554a39f32498
Birkir Karlsson <birkir.k@me.com>
2017-05-02 17:39:15 +0000

    Added log in system with usernames

--- CodeCur/CodeCur/Models/IdentityModels.cs
+++ CodeCur/CodeCur/Models/IdentityModels.cs
@@ -12,7 +12,6 @@ namespace CodeCur.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Password { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
