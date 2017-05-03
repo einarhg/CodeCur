1588a2ab26660443aa82a1684bc416fe1eb4c92c
Einar Helgi Guðmundsson <einar.h.gudm@gmail.com>
2017-05-03 15:02:29 +0000

    NavService expanded

--- CodeCur/CodeCur/Services/NavService.cs
+++ CodeCur/CodeCur/Services/NavService.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CodeCur.Models.Entities;
+using CodeCur.Models;
 
 namespace CodeCur.Services
 {
     public class NavService
     {
+        private static ApplicationDbContext _db;
+        public NavService()
+        {
+            _db = new ApplicationDbContext();
+        }
+        public static void AddProjectToDb(Project project)
+        {
+            // Add the new object to the Orders collection.
+            _db.Projects.Add(project);
+
+            // Fail check?
+            _db.SaveChanges();
+        }
     }
 }
