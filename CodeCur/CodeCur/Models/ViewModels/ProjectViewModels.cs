0e53f02b810733addabc2aebf0052aaae02b9fea
Einar Helgi Guðmundsson <einar.h.gudm@gmail.com>
2017-05-04 10:24:41 +0000

    fixed stupid

--- CodeCur/CodeCur/Models/ViewModels/ProjectViewModels.cs
+++ CodeCur/CodeCur/Models/ViewModels/ProjectViewModels.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:CodeCur/CodeCur/Models/ViewModels/ProjectViewModel.cs
 ﻿using CodeCur.Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -13,20 +12,4 @@ namespace CodeCur.Models.ViewModels
         public string UserName { get; set; }
         public string ProjectName { get; set; }
     }
-=======
-﻿using CodeCur.Models.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CodeCur.Models.ViewModels
-{
-    public class ProjectViewModel
-    {
-        public List<File> Files { get; set; }
-        public string UserName { get; set; }
-        public string ProjectName { get; set; }
-    }
->>>>>>> 4518c156d594061da6a4c47357f4d747c28f2838:CodeCur/CodeCur/Models/ViewModels/ProjectViewModels.cs
 }
