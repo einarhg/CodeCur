131ac151586a9ec4eba76646be43b048b758ec0b
Birkir Karlsson <birkir.k@me.com>
2017-05-10 14:07:33 +0000

    minor

--- CodeCur/CodeCur/Hubs/EditorHub.cs
+++ CodeCur/CodeCur/Hubs/EditorHub.cs
@@ -18,13 +18,16 @@ namespace CodeCur.Hubs
             Clients.Group(Convert.ToString(fileID), Context.ConnectionId).OnChange(changeData);
         }
 
-        public void listEditor(string name)
+        List<string> Editors = new List<string>();
+
+        public void listEditors(string name)
         {
             appendToEditorList(name);
         }
 
         public void appendToEditorList(string name)
         {
+            Editors.Add(name);
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<EditorHub>();
             context.Clients.All.appendToEditorList(name);
         }
