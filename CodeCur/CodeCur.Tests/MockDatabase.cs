78761144d10b6bc62e62f396068faa6e01e4d699
Einar Helgi Guðmundsson <einar.h.gudm@gmail.com>
2017-05-10 23:55:11 +0000

    added functional test files

---
+++ CodeCur/CodeCur.Tests/MockDatabase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FakeDbSet;
+using System.Data.Entity;
+using CodeCur.Models;
+using CodeCur.Models.Entities;
+
+namespace CodeCurr.Tests
+{
+    /// <summary>
+    /// This is an example of how we'd create a fake database by implementing the 
+    /// same interface that the BookeStoreEntities class implements.
+    /// </summary>
+    public class MockDatabase : IAppDataContext
+    {
+        /// <summary>
+        /// Sets up the fake database.
+        /// </summary>
+        public MockDatabase()
+        {
+            // We're setting our DbSets to be InMemoryDbSets rather than using SQL Server.
+            this.Projects = new InMemoryDbSet<Project>();
+            this.Files = new InMemoryDbSet<File>();
+            this.UserProjectRelations = new InMemoryDbSet<UserProjectRelation>();
+            this.Users = new InMemoryDbSet<ApplicationUser>();
+        }
+
+        public IDbSet<Project> Projects { get; set; }
+        public IDbSet<File> Files { get; set; }
+        public IDbSet<UserProjectRelation> UserProjectRelations { get; set; }
+        public IDbSet<ApplicationUser> Users { get; set; }
+
+        public int SaveChanges()
+        {
+            // Pretend that each entity gets a database id when we hit save.
+            int changes = 0;
+            //changes += DbSetHelper.IncrementPrimaryKey<Author>(x => x.AuthorId, this.Authors);
+            //changes += DbSetHelper.IncrementPrimaryKey<Book>(x => x.BookId, this.Books);
+
+            return changes;
+        }
+
+        public void Dispose()
+        {
+            // Do nothing!
+        }
+    }
+}
---
+++ CodeCur/CodeCur.Tests/Services/NavServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeCur.Services;
+using CodeCurr.Tests;
+using CodeCur.Models;
+using CodeCur.Models.Entities;
+using System.Collections.Generic;
+
+namespace CodeCur.Tests.Services
+{
+    [TestClass]
+    public class NavServiceTest
+    {
+        private NavService _service;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            var MockDb = new MockDatabase();
+
+            // Add users
+            var u1 = new ApplicationUser
+            {
+                Id = "a",
+                UserName = "nonni"
+            };
+            MockDb.Users.Add(u1);
+
+            // Add projects
+            var p1 = new Project
+            {
+                ID = 1,
+                UserID = "a",
+                Name = "proj",
+                Type = "Website",
+                Deleted = false
+            };
+
+            // Add relations
+            var r1 = new UserProjectRelation
+            {
+                ID = 1,
+                UserID = "a",
+                ProjectID = 1
+            };
+
+            // Add files
+            var f1 = new File
+            {
+                ID = 1,
+                ProjectID = 1,
+                Name = "file",
+                Type = "HTML",
+                //DateCreated = actual datetimer format,
+                Data = null,
+                Deleted = false
+            };
+
+
+            _service = new NavService(MockDb);
+        }
+
+        [TestMethod]
+        public void TestGetUserProjects()
+        {
+            // Arrange: 
+            const string userID = "a";
+
+            // Act:
+            List<Project> result = _service.GetUserProjects(userID);
+
+            // Assert:
+            Assert.AreEqual(1, result.Count);
+        }
+    }
+}
