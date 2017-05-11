5b00cbbc35ab01dfb47be3c9aa226412cda18d8d
Dagny Valgeirsdottir <dagnyhv@gmail.com>
2017-05-11 00:54:03 +0000

    Fixed password validation
    Special characters no longer needed for passwords

--- CodeCur/CodeCur/App_Start/IdentityConfig.cs
+++ CodeCur/CodeCur/App_Start/IdentityConfig.cs
@@ -54,7 +54,7 @@ namespace CodeCur
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
